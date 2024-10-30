9aa076273dd5df76320aa70aa83defa22b6d9d58
Kossi KOSSIVI <kossi.kossivi@etu.univ-amu.fr>
2024-10-30 17:40:02 +0100

    Ajout modélisation problème 1 Correction bug contrainte

--- src/cop/cop.py
+++ src/cop/cop.py
@@ -18,11 +18,28 @@ def generate_frequency_allocation_instance(fileName):
 
     num_stations = len(data["stations"])
     # regions = VarArray(size=num_stations, dom=[data["stations"][i]["region"] for i in range(num_stations)])
+
+    ############# Idée de modélisation du problème 1 #############
+    # Récupération d'une liste contenant toutes les valeurs possibles qu'une fréquence peut prendre
+    freqs_dom = []
+    for i in range(num_stations):
+        freqs_dom += data["stations"][i]["emetteur"] + data["stations"][i]["recepteur"]
+    freqs_dom = list(set(freqs_dom))  # occurrences uniques
+
+    dic_freq_id = {}  # dictionnaires qui associera à chaque fréquence un indice
+    for j in range(len(freqs_dom)):
+        dic_freq_id[freqs_dom[j]] = j
+    print(dic_freq_id)
+
+    # list de variables qui vérifient si une fréquence est utilisé ou pas
+    is_freq_used = VarArray(size=len(freqs_dom), dom=[0, 1])
+    ##############################################################
+
     emetteurs = VarArray(size=num_stations, dom=[data["stations"][i]["emetteur"] for i in range(num_stations)])
     recepteurs = VarArray(size=num_stations, dom=[data["stations"][i]["recepteur"] for i in range(num_stations)])
 
     satisfy(
-        [abs(emetteurs[i] - recepteurs[i]) == data["stations"]["delta"] for i in range(num_stations)],
+        [abs(emetteurs[i] - recepteurs[i]) == data["stations"][i]["delta"] for i in range(num_stations)],  # manquait le i
         [(abs(emetteurs[interference["x"]] - emetteurs[interference["y"]]) >= interference["Delta"])
          for interference in data["interferences"]],
 
@@ -53,6 +70,30 @@ def show_solution(solution):
 
     return
 
+
+def minimize_num_frequencies(e, r, f, dic_feq_id):
+    """
+    Prends deux Varray correspondant aux fréquences émettrices et récepteurs
+    et retourne une solution qui minimize le nombre de fréquences utilisé
+    """
+    # TODO: soit définir une nouvelle VarArray booléenne qui vaut vrai si une fréquence est utilisé et minimise la somme
+
+    satisfy(
+        [f[dic_feq_id[e_i]] == 1 for e_i in e],  # pas correct (génère une erreur)
+        [f[dic_feq_id[r_i]] == 1 for r_i in r]   # same
+    )
+
+    minimize(
+        Sum(f)
+    )
+
+    if solve() is SAT:
+        print(f"1 : Solution trouvée :\nÉmetteurs: {e.values()}\nRécepteurs: {r.values()}")
+        return
+
+    else:
+        print("1 : L'instance n'a pas de solution.")
+        return "NO SOLUTION"
 
 if __name__ == "__main__":
     startTime = time.time()
